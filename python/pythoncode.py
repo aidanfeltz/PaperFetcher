2c3eba03b6b5a5e789c6791c6324781c48ab2372
aidanfeltz <154996275+aidanfeltz@users.noreply.github.com>
2024-03-13 15:30:13 -0500

    Update pythoncode.py
    added code

--- python/pythoncode.py
+++ python/pythoncode.py
@@ -1,16 +1,42 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import os
 options = None
 driverchosen = input("type 1 for chrome, 2 for edge, 3 for firefox, and 4 for safari")
+urltoget = input("paste url of paper")
 while options is None:
 	if driverchosen == "1":
 		options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(options=options)
+    		driver = webdriver.Chrome(options=options)
 		continue
 	if driverchosen == "2":
-    options = webdrivers.EdgeOptions()
-    driver = webdriver.Edge(options=options)
+    		options = webdrivers.EdgeOptions()
+    		driver = webdriver.Edge(options=options)
 		continue
 	if driverchosen == "3":
 		options = webdriver.FirefoxOptions()
-    driver = webdriver.Firefox(options=options)
+    		driver = webdriver.Firefox(options=options)
 		continue
+issiteworking = False
+while issitework = False:
+	driver.get("https://sci-hub.se")
+	if "<form method=\"POST\" action=\"/\">" in driver.page_source:
+		issiteworking = True
+		continue
+	driver.close()
+	print("issue with https://sci-hub.se, now trying mirror site, sci-hub.ru")
+	driver.get("https://sci-hub.ru")
+	if "<form method=\"POST\" action=\"/\">" in driver.page_source:
+		issiteworking = True
+		continue
+	driver.close()
+	print("issue with sci-hub.ru, now trying mirror site, sci-hub.st")
+	driver.get("https://sci-hub.st")
+	
+	
+	
+	
+	
+		
+
+driver.quit()
